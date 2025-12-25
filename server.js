501209bac735463dd1d1038fcaf9998dc6dd6fd8
Aakash Solanki <Aaakash@synetalsolutions.com>
2025-12-25 15:02:04 +0530

    fixed all bugs

--- server.js
+++ server.js
@@ -45,6 +45,9 @@ app.use(cors({
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 // Routes
+app.use('/',()=>{
+    res.send("Welcome to Solar Backend");
+})
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/admin', require('./routes/adminRoutes'));
